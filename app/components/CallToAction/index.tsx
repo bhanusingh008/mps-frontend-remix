518e63993cd033cc22ff2b716b544863fef08198
bhanu <bhanusingh0605@gmail.com>
2025-01-13 13:43:48 +0530

    update number

--- app/components/CallToAction/index.tsx
+++ app/components/CallToAction/index.tsx
@@ -8,11 +8,11 @@ const CallToActionFooter = () => {
         <img className="icon" src={"assets/whatsapp-cta.svg"} alt="icon"></img>
       </a>
 
-      <a className="call action-button" href="tel:+918448203692">
+      <a className="call action-button" href="tel:+917048918618">
         <img className="icon" src={"assets/call.svg"} alt="icon"></img>
       </a>
 
-      <a href="sms:+918448203692" className="message action-button">
+      <a href="sms:+917048918618" className="message action-button">
         <img className="icon" src={"assets/message.svg"} alt="icon"></img>
       </a>
     </div>
