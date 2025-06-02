421b4566599fcbf058306494661ea75748a22859
bhanu <bhanusingh0605@gmail.com>
2025-06-02 22:33:33 +0530

    font size on notice page

--- app/components/Notice/Notice.jsx
+++ app/components/Notice/Notice.jsx
@@ -21,13 +21,13 @@ export const Notice = () => {
 
   return (
     <div className="sticky max-w-[1200px] mt-[-100px] sm:mt-[-170px] left-0">
-      <div className="mt-4 grid grid-cols-1 md:grid-cols-[1fr_5fr] p-8 bg-white border border-gray-200 shadow-[0_20px_500px_12px_#dedede] rounded-2xl min-h-[240px] box-border">
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-[1fr_5fr] p-4 sm:p-8 bg-white border border-gray-200 shadow-[0_20px_500px_12px_#dedede] rounded-2xl min-h-[240px] box-border">
         <div className="w-full items-center flex justify-center">
           <img src={"assets/pawan_sir.webp"} alt="Pawan Sir" loading="lazy" className="h-auto w-full max-w-[220px] rounded-full" />
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] bg-white text-primary text-[22px] md:text-[24px] p-5 md:p-2">
+        <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] bg-white text-primary sm:text-[22px] md:text-[24px] p-2 md:p-2">
           {/* Pointers */}
-          <div className="flex flex-col items-start font-bold text-[1.1rem] gap-2 md:items-center md:text-[18px]">
+          <div className="flex flex-col items-start font-bold text-[14px] sm:text-[16px] gap-2 md:items-center md:text-[18px]">
             <div className="w-full flex items-start gap-2">
               <img className="w-[30px]" src={"assets/star.svg"} alt="star" />
               <span>{21}+ Years of Experience</span>
